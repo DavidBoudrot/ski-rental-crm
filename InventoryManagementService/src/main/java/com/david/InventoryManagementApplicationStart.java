b7224e2980b1d500cfad1508335c3bd08c153305
david <davidboudrot2@gmail.com>
2024-12-27 13:06:43 -0500

    Server runing with local run config

--- InventoryManagementService/src/main/java/com/david/InventoryManagementApplicationStart.java
+++ InventoryManagementService/src/main/java/com/david/InventoryManagementApplicationStart.java
@@ -4,6 +4,8 @@ import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 
+// entry point into dropwizard projects is the extension of Application with a config passed in.
+// config can store variables
 public class InventoryManagementApplicationStart extends Application<InventoryManagementConfiguration> {
     public static void main(String[] args) throws Exception {
         new InventoryManagementApplicationStart().run(args);
--- InventoryManagementService/src/main/java/com/david/InventoryManagementConfiguration.java
+++ InventoryManagementService/src/main/java/com/david/InventoryManagementConfiguration.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.core.Configuration;
 import jakarta.validation.constraints.NotEmpty;
 
-public class InventoryManagementConfiguration  extends Configuration {
+public class InventoryManagementConfiguration extends Configuration {
     @NotEmpty
     private String template;
 
